3997d1b7d4f42528130d1626e6a91e646bae6d98
Nour <mohamednour25@gmail.com>
2024-06-23 20:21:58 +0300

    04

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,63 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextFeild: UITextField!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        weatherManager.delegate = self
+        searchTextFeild.delegate = self
     }
+    
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextFeild.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextFeild.endEditing(true)
+        return true
+        
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextFeild.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextFeild.text = ""
+    }
+    
+    func updateWeather(_ weatherManager: WeatherManager, weather: weatherModel) {
+        DispatchQueue.main.async { [self] in
+            temperatureLabel.text = weather.tempString
+            cityLabel.text = weather.cityName
+            conditionImageView.image = UIImage(systemName: weather.conditionName)
+
+        }
 
 
+    }
+    
+    func didFailWithError(error:Error) {
+        print(error)
+    }
 }
 
